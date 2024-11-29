24ded877f7388f8b072c51de35378f3e4c126516
A01710217 <a01710217@tec.mx>
2024-11-29 11:18:39 -0600

    feat(): crear pruebas unitarias con CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2
+# Build directory: C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[TestMain]=] "C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/build/test_main.exe")
+set_tests_properties([=[TestMain]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/CMakeLists.txt;57;add_test;C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
