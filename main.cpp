71b7618ae108c74e2f6f7d40b565d5cd263e075b
A01710217 <a01710217@tec.mx>
2024-11-28 23:49:33 -0600

    feat(main.cpp): agregar Parte3_M.cpp a main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <algorithm> // Para std::sort
+#include <queue>   // Para std::queue
 #include <climits>  // Para INT_MAX
 #include <cstring>  // Para memset
 
@@ -106,6 +107,65 @@ int tsp(const vector<vector<int>>& cost) {
 }
 
 
+// Función para realizar una búsqueda en anchura (BFS) y encontrar si existe un camino de aumento
+//Complejidad: O(n)
+bool bfs(const vector<vector<int>>& residualGraph, int source, int sink, vector<int>& parent) {
+    int V = residualGraph.size();
+    vector<bool> visited(V, false);
+    queue<int> q;
+    q.push(source);
+    visited[source] = true;
+    parent[source] = -1;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (int v = 0; v < V; ++v) {
+            if (!visited[v] && residualGraph[u][v] > 0) {  // Revisa si el nodo v no fue visitado y si es alcanzable desde u
+                if (v == sink) {
+                    parent[v] = u;
+                    return true;
+                }
+                q.push(v);
+                visited[v] = true;
+                parent[v] = u;
+            }
+        }
+    }
+    return false;
+}
+
+// Función para aplicar el algoritmo de Ford-Fulkerson para encontrar el flujo máximo desde s a t
+//Complejidad: O(n^2 * m)
+int fordFulkerson(vector<vector<int>> graph, int source, int sink) {
+    int u, v;
+    int V = graph.size();
+    vector<vector<int>> residualGraph = graph;  // Grafo residual donde residualGraph[u][v] indica capacidad residual de u a v
+    vector<int> parent(V);  // Almacenar el camino
+    int maxFlow = 0;  // Flujo máximo inicialmente es 0
+
+    // Aumenta el flujo mientras haya un camino de aumento
+    while (bfs(residualGraph, source, sink, parent)) {
+        // Encontrar la capacidad residual mínima de las aristas a lo largo del camino llenado por BFS
+        int pathFlow = INT_MAX;
+        for (v = sink; v != source; v = parent[v]) {
+            u = parent[v];
+            pathFlow = min(pathFlow, residualGraph[u][v]);
+        }
+
+        // Actualizar las capacidades residuales de las aristas y las aristas inversas a lo largo del camino
+        for (v = sink; v != source; v = parent[v]) {
+            u = parent[v];
+            residualGraph[u][v] -= pathFlow;
+            residualGraph[v][u] += pathFlow;
+        }
+
+        maxFlow += pathFlow;
+    }
+    return maxFlow;
+}
+
 
 //Función que calcuala la central más cercana a una colonia
 //Complejidad: O(n)
@@ -176,6 +236,9 @@ int main(){
     int costoMinimo = tsp(distancias);
 
     // 3. Calcular el flujo máximo
+    int source = 0;
+    int sink = N - 1;
+    int flujoMaximo = fordFulkerson(capacidades, source, sink);
 
     // 4. Encontrar la central más cercana
     int indiceCentral = centralMasCercana(nuevaCentral, centrales);
@@ -196,6 +259,7 @@ int main(){
     cout << endl;
 
     cout << "3." << endl;
+    cout << flujoMaximo << endl;
 
     cout << endl;
 
