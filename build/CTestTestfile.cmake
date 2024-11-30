214c2253748c6b59ec52850770065056eeb1d034
A01710217 <a01710217@tec.mx>
2024-11-29 21:27:37 -0600

    eliminar build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2
-# Build directory: C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AllTests]=] "C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/build/test_suite.exe")
-set_tests_properties([=[AllTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/CMakeLists.txt;60;add_test;C:/Users/axelc/OneDrive/Documentos/ADAA/ADAA_E2/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
